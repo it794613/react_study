1312ea60ca29e1c86f7155d375e344467fef499c
it794613 <it794613@naver.com>
2022-07-17 17:37:16 +0900

    리액트 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback ,useState } from 'react';
+import NewsPage from "./pages/NewPage";
+import { Route, Routes } from "../../../../../node_modules/react-router-dom/index";
 
-function App() {
+
+
+const App=()=>{
+  const[category,setCategory]=useState('all');
+  const onSelect = useCallback(category=> setCategory(category),[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<NewsPage/>}/>
+      <Route path='/:category' element={<NewsPage/>}/>
+    </Routes>
   );
-}
+};
 
 export default App;
